909914b1c94c06cb83cdac3bc78b1010ba85ed06
Kannu Mehta <kannumehta@gmail.com>
2025-05-01 20:19:36 -0700

    Some pip setup related changes

---
+++ fast_gemini/session/__init__.py
@@ -0,0 +1,7 @@
+from .ChatManager import ChatManager
+from .ChatMessage import ChatMessage
+from .ChatStorage import ChatStorage
+from .LocalChatStorage import LocalChatStorage
+from .GenerateContentRequest import GenerateContentRequest
+
+__all__ = ['ChatManager', 'ChatMessage', 'ChatStorage', 'LocalChatStorage', 'GenerateContentRequest'] 
